8dd259298fb819fffaf951193e38ad998bd0751a
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2026-05-25 17:23:12 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,52 @@
-import React, {FC} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import {Route, Routes} from 'react-router-dom';
 
-import CookieBanner from './components/CookieBanner';
-import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer';
-
-import TermsService from './pages/TermsService';
-import Sitemap from './pages/Sitemap';
 import Home from './pages/Home';
 import About from './pages/About';
 import Testimonials from './pages/Testimonials';
 import Schedule from './pages/Schedule';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import BlogPage from './pages/BlogPage';
-import FeaturedBlog from './blog/FeaturedBlog';
+import Blog from './pages/Blog';
+import BlogPost from './components/BlogPost';
 
+import CookieBanner from "./components/CookieBanner";
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsService from './pages/TermsService';
+import Sitemap from './pages/Sitemap';
+import ScrollToTop from "react-scroll-to-top";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const App: FC = () => {
+import "./types";
+
+const App: React.FC = () => {
     return (
-        <Router>
-            <div className="d-flex flex-column min-vh-100">
-                <NavigationBar/>
-                <main className="flex-grow">
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="about" element={<About/>}/>
-                        <Route path="testimonials" element={<Testimonials/>}/>
-                        <Route path="services" element={<Services/>}/>
-                        <Route path="schedule" element={<Schedule/>}/>
-                        <Route path="contact" element={<Contact/>}/>
-                        <Route path="blog" element={<BlogPage/>}/>
-                        <Route path="blog:id" element={<FeaturedBlog/>}/>
-
-                        <Route path="privacy-policy" element={<PrivacyPolicy/>}/>
-                        <Route path="terms-of-service" element={<TermsService/>}/>
-                        <Route path="sitemap" element={<Sitemap/>}/>
-                    </Routes>
-                </main>
-                <Footer/>
-                <CookieBanner/>
+        <div className="d-flex flex-column min-vh-100">
+            <NavigationBar/>
+            <main className="flex-grow-1">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/testimonials" element={<Testimonials/>}/>
+                    <Route path="/services" element={<Services/>}/>
+                    <Route path="/schedule" element={<Schedule/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/blog" element={<Blog/>}/>
+                    <Route path="/blog/:id" element={<BlogPost/>}/>
+                    <Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
+                    <Route path="/terms-of-service" element={<TermsService/>}/>
+                    <Route path="/sitemap" element={<Sitemap/>}/>
+                </Routes>
+            </main>
+            <CookieBanner/>
+            <Footer/>
+            <ScrollToTop/>
             </div>
-        </Router>
-
     );
-};
-export default App;
+}
 
+export default App;

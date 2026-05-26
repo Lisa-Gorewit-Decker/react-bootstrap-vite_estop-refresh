57bcb8ef58d46f475500845010249d1b80501f0a
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2026-05-25 17:47:50 -0700

    Add files via upload
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,27 +1,29 @@
-//MainLayout.tsz
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 import NavigationBar from '../components/NavigationBar';
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
+import CookieBanner from "../components/CookieBanner";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import './App.css';
+import "./index.css";
 
 const MainLayout: React.FC = () => {
     return (
-            <div className="d-flex flex-column min-vh-100">
-                <NavigationBar/>
-                <main className="flex-grow">
-                    <Container>
-                        <Outlet/>
-                    </Container>
-                </main>
-                <Footer/>
-            </div>
+        <div className="d-flex flex-column min-vh-100">
+            <NavigationBar/>
+            <main className="flex-grow-1">
+                <Container>
+                    <Outlet/>
+                </Container>
+            </main>
+            <Footer/>
+            <CookieBanner/>
+        </div>
     );
-};
+}; // <--- Added the missing closing brace and semicolon here
+
 export default MainLayout;

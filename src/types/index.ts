2b12da470b274fcd005e081d1ab7a3ac0dec92d7
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2026-05-25 17:25:59 -0700

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,14 @@
-export interface DrivingService {
-    id: string;
-    title: string;
-    description: string;
-    price: string;
-    date: string;
-    time: string;
-    location: string;
-}
-export interface BlogPost {
-    id: number;
-    title: string;
-    excerpt: string;
-    date: string;
-    slug: string;
-}
-
 export interface ContactState {
     fullName: string;
     phone: string;
     message: string;
 }
-export interface DayCounterProps {
+export interface Services {
+    id: string;
+    title: string;
+    description: string;
+}
+interface DayCounterProps {
     startDateIso: string;
-    endDateIso: string;
+    label: string;
 }
